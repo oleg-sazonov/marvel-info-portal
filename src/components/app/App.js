08b664f7bfe5cfa297c8c7adbc0f30bfa76b6246
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-07-06 19:34:35 +0300

    add charInfo(.js + .scss); add charList(.js + .scss)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,9 @@
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from "../../resources/img/bg-hero.png";
 
 
 function App() {
@@ -8,6 +12,11 @@ function App() {
             <AppHeader />
             <main>
                 <RandomChar />
+                <div className="char__content">
+                    <CharList />
+                    <CharInfo />
+                    <img src={decoration} alt="bg-hero" className="bg-decoration"/>
+                </div>
             </main>
         </div>
     );
