2d938cfeadefd06d88d523389dc7f983ec244462
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-07-06 16:15:13 +0300

    create: marvel-info-portal

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import AppHeader from "../appHeader/AppHeader";
+
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader/>
+        </div>
+    )
+}
+
+export default App;
