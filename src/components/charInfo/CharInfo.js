20645ca7daac265cb3cc8ef5f7175d5123203580
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-07-21 21:01:22 +0300

    feat(CharList/CharInfo.js): add type validation using PropTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -115,4 +117,9 @@ const View = ({char}) => {
 	</>
 	)
 }
+
+CharInfo.propTypes = {
+	charId: PropTypes.number
+}
+
 export default CharInfo;
